aaf207993ac9b57d8d20e57e12c9fb86ed73de35
Дмитрий Быков <dimkson@yahoo.com>
2020-05-06 13:34:10 +0300

    Реализовано Задание 1.

--- Lesson4/Lesson4/Program.cs
+++ Lesson4/Lesson4/Program.cs
@@ -12,9 +12,30 @@ namespace Lesson4
             Menu menu = new Menu(delMenus);
             menu.ChooseMenu();
         }
+        #region Задание 1
         static void Task01()
         {
-
+            //Найти количество пар эл-ов, в которых хотя бы одно из чисел делится на 3.
+            Random rnd = new Random();
+            int[] arr = new int[20];
+            for(int i = 0; i < 20; i++)
+                arr[i] = rnd.Next(-10000, 10000);
+            Console.WriteLine("Исходные данные:");
+            foreach(int i in arr)
+                Console.Write(i + " ");
+            Console.WriteLine("\n");
+            int count = 0;
+            for (int i = 0; i < 19; i++)
+            {
+                if(arr[i]%3==0 || arr[i + 1] % 3 == 0)
+                {
+                    count++;
+                    Console.WriteLine("Пара эл-ов: " + arr[i] + " " + arr[i + 1]);
+                }
+            }
+            Console.WriteLine("\nКоличество пар: " + count);
+            FC.Pause();
         }
+        #endregion
     }
 }

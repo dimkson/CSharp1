da1068f4605901959e4e03f0c8aa780d12b056e0
dimkson <dimkson@yahoo.com>
2020-05-20 16:43:19 +0300

    Добавлено Задание 2.

--- Lesson6/Lesson6/Program.cs
+++ Lesson6/Lesson6/Program.cs
@@ -12,7 +12,7 @@ namespace Lesson6
     {
         static void Main(string[] args)
         {
-            Menu.delMenu[] delMenu = new Menu.delMenu[] {Task1};
+            Menu.delMenu[] delMenu = new Menu.delMenu[] { Task1, Task2 };
             Menu menu = new Menu(delMenu);
             menu.ChooseMenu();
         }
@@ -24,6 +24,27 @@ namespace Lesson6
             Task01.Table(Task01.MyMethod1, 10, -2, 2);
             Console.WriteLine("Таблица функции A*sin(x)");
             Task01.Table(Task01.MyMethod2, 10, -2, 2);
+            FC.Pause();
+        }
+        #endregion
+
+        #region Задание 2
+        static void Task2()
+        {
+            Task02.delFun[] delFuns = new Task02.delFun[] { Math.Sin, Math.Cos, Math.Tan, Math.Atan, Task02.MyMethod };
+            Task02.delFun fun = Task02.ChooseMenu(delFuns);
+
+            Console.WriteLine("Введите A:");
+            Int32.TryParse(Console.ReadLine(), out int a);
+            Console.WriteLine("Введите B:");
+            Int32.TryParse(Console.ReadLine(), out int b);
+
+            Task02.SaveFun("data.bin", fun , a, b, 2);
+            double[] arr = Task02.Load("data.bin", out double min);
+            foreach(double db in arr)
+            {
+                Console.Write(db + " ");
+            }
             FC.Pause();
         }
         #endregion

c2a6a073b8c98158f3d76b72d2be69beb17de210
dimkson <dimkson@yahoo.com>
2020-05-20 12:43:48 +0300

    Подключена библиотека классов MenuLib

---
+++ Lesson6/Lesson6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MenuLib;
+using FC = MenuLib.FastConsole;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.delMenu[] delMenu = new Menu.delMenu[] {Task01};
+            Menu menu = new Menu(delMenu);
+            menu.ChooseMenu();
+        }
+        static void Task01()
+        {
+
+        }
+
+    }
+}

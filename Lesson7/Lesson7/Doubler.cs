dce5458161d67fe9937fd7355d671d9ddf6c7a59
dimkson <dimkson@yahoo.com>
2020-05-26 13:28:22 +0300

    Реализована форма Удвоитель

---
+++ Lesson7/Lesson7/Doubler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7
+{
+    class Doubler
+    {
+        public int Current { get; private set; }
+        public int Max { get; private set; }
+        public int Count { get; private set; }
+        public int MinimumStep { get; private set; }
+        Stack<int> stack = new Stack<int>();
+
+        public Doubler()
+        {
+            Current = 1;
+            Count = 0;
+            Max = new Random().Next(10, 100);
+            MinimumStep = MinStep();
+        }
+
+        public int MinStep()
+        {
+            int max = Max;
+            int step = 0;
+
+            while (max != 1)
+            {
+                if (max % 2 != 0)
+                {
+                    max--;
+                    step++;
+                }
+                else
+                {
+                    max /= 2;
+                    step++;
+                }
+            }
+            return step;
+        }
+
+        public void Plus()
+        {
+            stack.Push(Current);
+            Current++;
+            Count++;
+        }
+        public void Multi()
+        {
+            stack.Push(Current);
+            Current *= 2;
+            Count++;
+        }
+        public void Reset()
+        {
+            stack.Clear();
+            Current = 1;
+            Count = 0;
+        }
+        public void Cancel()
+        {
+            if (stack.Count != 0)
+            {
+                Current = stack.Pop();
+                Count--;
+            }
+        }
+
+    }
+}
--- Lesson7/Lesson7/Program.cs
+++ Lesson7/Lesson7/Program.cs
@@ -10,13 +10,20 @@ namespace Lesson7
 {
     class fDouble: Form
     {
-        private Button bPlus, bMulti, bReset;
-        private Label lblCurrent;
+        private Button bPlus, bMulti, bReset, bCancel;
+        private Label lblCurrent, lblCount;
         private TextBox tbMax;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem tsmiNew, tsmiNewGame, tsmiExit;
+
+        Doubler doubler;
 
         public fDouble()
         {
             InitializeComponent();
+            doubler = new Doubler();
+            tbMax.Text = doubler.Max.ToString();
+            UpdateInfo();
         }
 
         private void InitializeComponent()
@@ -24,49 +31,140 @@ namespace Lesson7
             this.bPlus = new Button();
             this.bMulti = new Button();
             this.bReset = new Button();
+            this.bCancel = new Button();
             this.lblCurrent = new Label();
             this.tbMax = new TextBox();
+            this.lblCount = new Label();
+            this.menuStrip = new MenuStrip();
+            this.tsmiNew = new ToolStripMenuItem();
+            this.tsmiNewGame = new ToolStripMenuItem();
+            this.tsmiExit = new ToolStripMenuItem();            
 
             this.SuspendLayout();
+            this.menuStrip.SuspendLayout();
             //
             // bPlus
             //
             this.bPlus.Name = "bPlus";
             this.bPlus.Text = "+1";
-            this.bPlus.Location = new Point(10, 20);
+            this.bPlus.Location = new Point(10, 30);
+            this.bPlus.Click += BPlus_Click;
             //
             // bMulti
             //
             this.bMulti.Name = "bMulti";
             this.bMulti.Text = "x2";
-            this.bMulti.Location = new Point(10, 60);
+            this.bMulti.Location = new Point(10, 70);
+            this.bMulti.Click += BMulti_Click;
             //
             // bReset
             //
             this.bReset.Name = "bReset";
             this.bReset.Text = "Сброс";
-            this.bReset.Location = new Point(10, 100);
+            this.bReset.Location = new Point(10, 110);
+            this.bReset.Click += BReset_Click;
+            //
+            // bCancel
+            //
+            this.bCancel.Name = "bCancel";
+            this.bCancel.Text = "Отменить";
+            this.bCancel.Location = new Point(10, 150);
+            this.bCancel.Click += BCancel_Click;
             //
             // lblCurrent
             //
             this.lblCurrent.Name = "lblCurrent";
             this.lblCurrent.Text = "1";
-            this.lblCurrent.Location = new Point(120, 23);
+            this.lblCurrent.Location = new Point(120, 34);
             //
             // tbMax
             //
             this.tbMax.Name = "tbMax";
             this.tbMax.Text = "";
-            this.tbMax.Location = new Point(120, 60);
+            this.tbMax.Location = new Point(120, 70);
+            //
+            // lblCount
+            //
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Text = "0";
+            this.lblCount.Location = new Point(120, 114);
+            //
+            // menuStrip
+            //
+            this.menuStrip.Items.Add(tsmiNew);
+            this.menuStrip.Name = "menuStrip";
+            //
+            // tsmiNew
+            //
+            this.tsmiNew.Text = "New";
+            this.tsmiNew.DropDownItems.AddRange(new ToolStripItem[] { tsmiNewGame,
+                tsmiExit });
+            //
+            // tsmiNewGame
+            //
+            this.tsmiNewGame.Text = "New Game";
+            this.tsmiNewGame.Click += TsmiNewGame_Click;
+            //
+            // tsmiExit
+            //
+            this.tsmiExit.Text = "Exit";
+            this.tsmiExit.Click += TsmiExit_Click;
+            
             // 
             // fDouble
             // 
             this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Controls.AddRange(new Control[] { bPlus, bMulti, bReset, lblCurrent, tbMax });
+            this.Controls.AddRange(new Control[] { bPlus, bMulti, bReset, bCancel,
+                lblCurrent, tbMax, lblCount, menuStrip });
+            this.MainMenuStrip = this.menuStrip;
             this.Text = "Удвоитель";
             this.Name = "fDouble";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
+        }
+
+        private void BCancel_Click(object sender, EventArgs e)
+        {
+            doubler.Cancel();
+            UpdateInfo();
+        }
+
+        private void TsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TsmiNewGame_Click(object sender, EventArgs e)
+        {
+            doubler = new Doubler();
+            tbMax.Text = doubler.Max.ToString();
+            UpdateInfo();
+        }
+
+        private void BReset_Click(object sender, EventArgs e)
+        {
+            doubler.Reset();
+            UpdateInfo();
+        }
+
+        private void BMulti_Click(object sender, EventArgs e)
+        {
+            doubler.Multi();
+            UpdateInfo();
+        }
+
+        private void BPlus_Click(object sender, EventArgs e)
+        {
+            doubler.Plus();
+            UpdateInfo();
+        }
+        private void UpdateInfo()
+        {
+            this.lblCurrent.Text = doubler.Current.ToString();
+            this.lblCount.Text = $"{doubler.Count.ToString()} из {doubler.MinimumStep}";
         }
     }
 
